d034c76ce5f6a7ca1a9475db301f36207d9055c3
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-17 13:15:20 -0700

    .

--- Busqueda/Lineal.cs
+++ Busqueda/Lineal.cs
@@ -10,7 +10,23 @@ namespace PracCla_Erick_EESA.Datos
     {
         public void uno() 
         {
-            
+            int[] a = { 4, 8, 10, 12, 13, 14, 16, 28, 25, 30 };
+            int inicio = 0;
+            int fin = a.Length;
+            int medio = (inicio + fin +1) / 2;
+            int indice = -1;
+            Console.WriteLine("Dame un numero para buscar");
+            int buscar = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[medio] == indice)
+                    indice = medio;
+                else if (a[indice] > buscar)
+                    fin = medio - 1;
+                    medio = inicio = (inicio + fin) / 2;
+                else
+                break;
+            }
         }
     }
 }
